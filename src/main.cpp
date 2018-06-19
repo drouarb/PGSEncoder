313d6f883500f26ad76732cf9ad02372380441ef
Benjamin DROUARD <benjamin.drouard@epitech.eu>
2018-06-19 16:57:16 +0200

    Generating a valid .sup

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <zconf.h>
 #include "PGSStructures.h"
 
-void generatePCS(int fd, int width, int height, int composition_number) {
+void generatePCS(int fd, int pts, int width, int height, int composition_number) {
     PGSHeader_t         header;
     PCSSegment_t        segment;
     CompositionObject_t compositionObject;
@@ -23,7 +23,7 @@ void generatePCS(int fd, int width, int height, int composition_number) {
     compositionObject.object_y = 0;
     compositionObject.object_x = 0;
 
-    header.pts = 100;
+    header.pts = pts;
     header.segment_type = SEGMENT_TYPE_PCS;
     header.segment_size = sizeof(segment) + sizeof(compositionObject);
 
@@ -32,7 +32,7 @@ void generatePCS(int fd, int width, int height, int composition_number) {
     write_struct(fd, compositionObject);
 }
 
-void generateWDS(int fd) {
+void generateWDS(int fd, int pts) {
     PGSHeader_t  header;
     WDSSegment_t window;
 
@@ -43,7 +43,7 @@ void generateWDS(int fd) {
     window.width = 100;
     window.height = 100;
 
-    header.pts = 100;
+    header.pts = pts;
     header.segment_type = SEGMENT_TYPE_WDS;
     header.segment_size = sizeof(window);
 
@@ -51,30 +51,38 @@ void generateWDS(int fd) {
     write_struct(fd, window);
 }
 
-void generatePDS(int fd) {
+void generatePDS(int fd, int pts) {
     PGSHeader_t  header;
     PDSSegment_t pds;
-    PDSPalette_t palette;
+    PDSPalette_t palette1;
+    PDSPalette_t palette2;
 
     pds.palette_id = 0;
     pds.palette_version = 0;
 
-    palette.palette_entry_id = 0;
-    palette.y = 10;
-    palette.cb = 80;
-    palette.cr = 80;
-    palette.alpha = 0;
+    palette1.palette_entry_id = 0;
+    palette1.y = 0x80;
+    palette1.cb = 0x80;
+    palette1.cr = 0x80;
+    palette1.alpha = 0xFF;
 
-    header.pts = 100;
+    palette2.palette_entry_id = 1;
+    palette2.y = 0x80;
+    palette2.cb = 0x80;
+    palette2.cr = 0x80;
+    palette2.alpha = 0x80;
+
+    header.pts = pts;
     header.segment_type = SEGMENT_TYPE_PDS;
-    header.segment_size = sizeof(pds) + sizeof(palette);
+    header.segment_size = sizeof(pds) + sizeof(palette1) * 2;
 
     write_struct(fd, header);
     write_struct(fd, pds);
-    write_struct(fd, palette);
+    write_struct(fd, palette1);
+    write_struct(fd, palette2);
 }
 
-void generateODS(int fd) {
+void generateODS(int fd, int pts, int color) {
     PGSHeader_t  header;
     ODSSegment_t object;
 
@@ -84,7 +92,7 @@ void generateODS(int fd) {
     object.width = 100;
     object.height = 100;
 
-    header.pts = 100;
+    header.pts = pts;
     header.segment_type = SEGMENT_TYPE_ODS;
     header.segment_size = sizeof(object) + (600);
 
@@ -104,7 +112,7 @@ void generateODS(int fd) {
         code = 0x64;
         write_struct(fd, code);
 
-        code = 0;
+        code = color;
         write_struct(fd, code);
 
         //================
@@ -116,22 +124,46 @@ void generateODS(int fd) {
     }
 }
 
-void generateEND(int fd) {
+void generateEND(int fd, int pts) {
     PGSHeader_t header;
 
-    header.pts = 100000;
+    header.pts = pts;
     header.segment_type = SEGMENT_TYPE_END;
     header.segment_size = 0;
 
     write_struct(fd, header);
 }
 
+void cleanScreen(int fd, int pts) {
+    PGSHeader_t         header;
+    PCSSegment_t        segment;
+
+    segment.width = 1280;
+    segment.height = 720;
+    segment.composition_number = 1;
+
+    segment.composition_state = COMPOSITION_STATE_NORMAL;
+    segment.palette_update_flag = PALETTE_UPDATE_FLAG_FALSE;
+    segment.palette_id = 0;
+    segment.composition_object_count = 0;
+
+    header.pts = pts;
+    header.segment_type = SEGMENT_TYPE_PCS;
+    header.segment_size = sizeof(segment);
+
+    write_struct(fd, header);
+    write_struct(fd, segment);
+    generateEND(fd, pts);
+}
+
 int main() {
     int fd = open("out.sup", O_WRONLY | O_CREAT | O_APPEND, S_IRWXU);
-    generatePCS(fd, 1920, 1080, 1);
-    generateWDS(fd);
-    generatePDS(fd);
-    generateODS(fd);
-    generateEND(fd);
+    generatePCS(fd, 100, 1280, 720, 1);
+    generateWDS(fd, 100);
+    generatePDS(fd, 100);
+    generateODS(fd, 100, 0);
+    generateEND(fd, 100);
+
+    cleanScreen(fd, 10000000);
     close(fd);
 }
